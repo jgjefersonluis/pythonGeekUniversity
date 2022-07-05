d8b79fbd77278b827b8565211be9bda2865c749b
jgjefersonluis <jgjefersonluis@gmail.com>
2022-07-05 16:39:47 -0300

    convertendo listas em strings

---
+++ secao7_colecoes/listasDevFuria/convertendoListasEmStrings.py
@@ -0,0 +1,12 @@
+list = ['primeiro', 'segundo', 'terceiro']
+
+print(list)
+print(','.join(list))
+
+listNumber = [10, 20, 30]
+
+print(str(listNumber))
+print(str(listNumber).strip('[]'))
+print(str(listNumber)[1:-1])
+print('-'.join(map(str, listNumber)))
+
---
+++ secao7_colecoes/listas_learndatasci/favoritos.py
@@ -0,0 +1,5 @@
+favoritos_tres_games = ['Call of Duty', 'Final Fantasy', 'Mass Effect']
+
+escolha = int(input('Which list index would you like to pick (0, 1, or 2)? '))
+
+print(favoritos_tres_games[escolha])
